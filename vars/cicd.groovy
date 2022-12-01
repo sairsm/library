32028a69a9773ea543632e8c892f33f061b83e94
sairsm <117734937+sairsm@users.noreply.github.com>
2022-12-01 21:54:36 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -3,3 +3,11 @@ def newDownload(repo)
 {
 git "https://github.com/sairsm/${repo}"
 }
+def newBuild()
+{
+  sh 'mvn package'
+}
+def newDeploy(jobname,ip,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@172.31.2.205:/var/lib/tomcat9/webapps/testapp.war"
+}
